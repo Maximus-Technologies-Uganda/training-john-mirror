d2ccae03c425d0bb4633a5ff662a4cc15ccbe628
johnkayiira-jpg <your_email@example.com>
2025-10-10 17:30:46 +0300

    feat(tests): add CLI test and debug scripts for todo functionality

--- todo/src/todo-cli.js
+++ todo/src/todo-cli.js
@@ -1,12 +1,3 @@
 export { run } from '../../src/todo-cli.js';
 
-if (import.meta.url === `file://${process.argv[1]}` ||
-  (process.argv[1] && import.meta.url.endsWith('todo-cli.js') && process.argv[1].endsWith('todo-cli.js')))
-{
-  import('../../src/todo-cli.js').then(({ run }) => {
-    const exitCode = run(process.argv);
-    process.exit(exitCode);
-  });
-}
-
 
